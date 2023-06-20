046bbccdba17eababa65c31c11a9fa75ab0a6323
vaweto <vagelisbisbikis@gmail.com>
2023-06-20 16:40:59 +0300

    migrations of main entities

---
+++ database/factories/MenuFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Menu>
+ */
+class MenuFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->name,
+            'description' => $this->faker->text,
+            'image' => $this->faker->firstName,
+            'price' => $this->faker->randomFloat(2,0,10000)
+        ];
+    }
+}
